21b2fa0ee12a5905e413025349221f6001b6f88d
theo <lesgooo1993@proton.me>
2023-09-14 14:59:11 +0530

    added back arrow to changelog

--- src/routes/changelog/+page.svelte
+++ src/routes/changelog/+page.svelte
@@ -2,7 +2,13 @@
     import { logs } from '$lib/logs.js'
 </script>
 
-<h1 class="font-bold text-3xl mb-2">changelog.</h1>
+<div class="title flex items-center gap-2 mb-2">
+    <a href="/">
+        <svg class="h-6 w-6 mt-1" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h14m-7-7l-7 7l7 7"/></svg>
+    </a>
+
+    <h1 class="font-bold text-3xl">changelog.</h1>
+</div>
 
 <p class="mb-2"><b>WARNING:</b> noob code hehe</p>
 
